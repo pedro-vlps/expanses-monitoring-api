1971f77e9543e62f3dd4cdeb0b8ee21d14f16183
Pedro Vieira <pedro515vieira@gmail.com>
2026-04-13 22:03:34 -0300

    [feature] - Added new tables to the database and changed db files

--- src/databases/drop_db.sql
+++ src/databases/drop_db.sql
@@ -1,3 +1,6 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS custom_expenses_groups;
+DROP TABLE IF EXISTS custom_expenses_groups_expenses;
 DROP TABLE IF EXISTS expenses;
+DROP TABLE IF EXISTS custom_expenses_groups;
+DROP TABLE IF EXISTS banks_users;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS banks;
--- src/databases/generate_db.sql
+++ src/databases/generate_db.sql
@@ -13,14 +13,40 @@ CREATE TABLE IF NOT EXISTS custom_expenses_groups (
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS banks (
+  id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  cnpj TEXT NOT NULL,
+  code TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS banks_users (
+  id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  bank_id uuid NOT NULL,
+  user_id uuid NOT NULL,
+  account_balance DECIMAL(10, 2) NOT NULL,
+  credit_card_expiration_date DATE,
+  FOREIGN KEY (bank_id) REFERENCES banks(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS expenses (
   id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   user_id uuid NOT NULL,
+  bank_id uuid NOT NULL,
   amount DECIMAL(10, 2) NOT NULL,
   category TEXT,
   description TEXT,
   date DATE NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (bank_id) REFERENCES banks(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS custom_expenses_groups_expenses (
+  id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  custom_expenses_group_id uuid NOT NULL,
+  expense_id uuid NOT NULL,
+  FOREIGN KEY (custom_expenses_group_id) REFERENCES custom_expenses_groups(id) ON DELETE CASCADE,
+  FOREIGN KEY (expense_id) REFERENCES expenses(id) ON DELETE CASCADE
+);
